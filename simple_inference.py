39f1ebb17ce385482848ef36908a5632c2cf407e
Alex Wortega <alexdragannikolich@gmail.com>
2021-10-24 17:30:15 +0300

    Update simple_inference.py

--- simple_inference.py
+++ simple_inference.py
@@ -1,5 +1,14 @@
 from helpers import max_cosine_images, max_cosine_text
 import os
-os.system("wget https://upload.wikimedia.org/wikipedia/commons/5/53/2008-03-11_Bicyclist_in_Carrboro.jpg ")
+try:
+  os.system("wget https://upload.wikimedia.org/wikipedia/commons/5/53/2008-03-11_Bicyclist_in_Carrboro.jpg ")
+  os.system("wget https://upload.wikimedia.org/wikipedia/commons/2/2f/Prm_VJ_fig1_featureTypesWithAlpha.png")
+execept:
+  print('что то пошло не так')
 
 print(max_cosine_text('2008-03-11_Bicyclist_in_Carrboro.jpg',[ 'мотоцикл','велосипед', 'крсное вино']))#велосипед
+
+_, _ ,path = max_cosine_images(['/content/Prm_VJ_fig1_featureTypesWithAlpha.png','/content/2008-03-11_Bicyclist_in_Carrboro.jpg'],'велосипед')
+
+print(path)
+
